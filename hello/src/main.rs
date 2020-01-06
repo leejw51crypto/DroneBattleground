cf18a5bc1f2ecd4e5b854937d75effef2c2e041d
jongwhan lee <jonghwan@crypto.com>
2020-01-06 23:32:34 +0800

    ok

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -24,10 +24,4 @@ mod tests {
         assert_eq!(add(1, 2), 3);
     }
 
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
-    }
 }
